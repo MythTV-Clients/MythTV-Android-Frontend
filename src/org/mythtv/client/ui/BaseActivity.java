566b743a03a7af76b4abfa9a82546ef517b1b58b
Daniel Frey <dfrey@knoll.com>
2012-04-25 09:16:28 -0400

    updated license so all files are GPLv3

--- src/org/mythtv/client/ui/BaseActivity.java
+++ src/org/mythtv/client/ui/BaseActivity.java
@@ -1,3 +1,24 @@
+/**
+ *  This file is part of MythTV for Android
+ * 
+ *  MythTV for Android is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MythTV for Android is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MythTV for Android.  If not, see <http://www.gnu.org/licenses/>.
+ *   
+ * @author Daniel Frey <dmfrey at gmail dot com>
+ * 
+ * This software can be found at <https://github.com/dmfrey/mythtv-for-android/>
+ *
+ */
 package org.mythtv.client.ui;
 
 import org.mythtv.client.MainApplication;
@@ -8,8 +29,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 
 /**
  * A base activity that defers common functionality across app activities to an
--- src/org/mythtv/client/ui/setup/capture/CaptureCardsActivity.java
+++ src/org/mythtv/client/ui/setup/capture/CaptureCardsActivity.java
@@ -1,5 +1,23 @@
 /**
+ *  This file is part of MythTV for Android
  * 
+ *  MythTV for Android is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MythTV for Android is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MythTV for Android.  If not, see <http://www.gnu.org/licenses/>.
+ *   
+ * @author Daniel Frey <dmfrey at gmail dot com>
+ * 
+ * This software can be found at <https://github.com/dmfrey/mythtv-for-android/>
+ *
  */
 package org.mythtv.client.ui.setup.capture;
 
@@ -9,7 +27,6 @@ import org.mythtv.client.ui.AbstractMythListActivity;
 import org.mythtv.client.ui.setup.SetupActivity;
 import org.mythtv.services.api.capture.CaptureCard;
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
