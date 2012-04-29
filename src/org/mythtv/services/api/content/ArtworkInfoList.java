0a862fc389143f66c980d764f8d8f480d7e6e30e
Daniel Frey <dfrey@knoll.com>
2012-04-29 11:33:31 -0400

    added content objects and derivative list objects

---
+++ src/org/mythtv/services/api/content/ArtworkInfoList.java
@@ -0,0 +1,70 @@
+/**
+ *  This file is part of MythTV for Android
+ * 
+ *  MythTV for Android is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MythTV for Android is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MythTV for Android.  If not, see <http://www.gnu.org/licenses/>.
+ *   
+ * @author Daniel Frey <dmfrey at gmail dot com>
+ * 
+ * This software can be found at <https://github.com/dmfrey/mythtv-for-android/>
+ *
+ */
+package org.mythtv.services.api.content;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+/**
+ * @author Daniel Frey
+ *
+ */
+public class ArtworkInfoList {
+
+	@JsonProperty( "ArtworkInfoList" )
+	private ArtworkInfos artworkInfos;
+	
+	public ArtworkInfoList() { }
+
+	/**
+	 * @return the artworkInfos
+	 */
+	public ArtworkInfos getArtworkInfos() {
+		return artworkInfos;
+	}
+
+	/**
+	 * @param artworkInfos the artworkInfos to set
+	 */
+	public void setArtworkInfos( ArtworkInfos artworkInfos ) {
+		this.artworkInfos = artworkInfos;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append( "ArtworkInfoList [" );
+		
+		if( artworkInfos != null ) {
+			builder.append( "artworkInfos=" );
+			builder.append( artworkInfos );
+		}
+		
+		builder.append( "]" );
+	
+		return builder.toString();
+	}
+	
+}
---
+++ src/org/mythtv/services/api/content/LiveStreamInfos.java
@@ -0,0 +1,69 @@
+/**
+ *  This file is part of MythTV for Android
+ * 
+ *  MythTV for Android is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  MythTV for Android is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with MythTV for Android.  If not, see <http://www.gnu.org/licenses/>.
+ *   
+ * @author Daniel Frey <dmfrey at gmail dot com>
+ * 
+ * This software can be found at <https://github.com/dmfrey/mythtv-for-android/>
+ *
+ */
+package org.mythtv.services.api.content;
+
+import java.util.List;
+
+/**
+ * @author Daniel Frey
+ *
+ */
+public class LiveStreamInfos {
+
+	private List<LiveStreamInfo> liveStreamInfos;
+	
+	public LiveStreamInfos() { }
+
+	/**
+	 * @return the liveStreamInfos
+	 */
+	public List<LiveStreamInfo> getLiveStreamInfos() {
+		return liveStreamInfos;
+	}
+
+	/**
+	 * @param liveStreamInfos the liveStreamInfos to set
+	 */
+	public void setLiveStreamInfos( List<LiveStreamInfo> liveStreamInfos ) {
+		this.liveStreamInfos = liveStreamInfos;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append( "LiveStreamInfos [" );
+		
+		if( liveStreamInfos != null ) {
+			builder.append( "liveStreamInfos=" );
+			builder.append( liveStreamInfos );
+		}
+		
+		builder.append( "]" );
+	
+		return builder.toString();
+	}
+	
+}
