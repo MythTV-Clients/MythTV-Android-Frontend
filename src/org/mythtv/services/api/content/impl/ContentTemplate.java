8e2037fa34701e56b8da3cb9afdb97e7a9d4e178
Daniel Frey <dfrey@knoll.com>
2012-05-16 20:47:45 -0400

    fixed date format

--- src/org/mythtv/services/api/content/impl/ContentTemplate.java
+++ src/org/mythtv/services/api/content/impl/ContentTemplate.java
@@ -40,7 +40,7 @@ import org.springframework.web.client.RestTemplate;
  */
 public class ContentTemplate extends AbstractContentOperations implements ContentOperations {
 
-	private static final SimpleDateFormat sdf = new SimpleDateFormat( "yyyy-MM-ddTHH:mm:ss" );
+	private static final SimpleDateFormat sdf = new SimpleDateFormat( "yyyy-MM-dd'T'HH:mm:ss" );
 	
 	private final RestTemplate restTemplate;
 	
