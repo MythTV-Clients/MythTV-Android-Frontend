b98a403520d7af68d75102195bd911e98512fc58
Daniel Frey <dfrey@knoll.com>
2012-05-23 21:21:04 -0400

    added video api implementation

--- src/org/mythtv/services/api/video/impl/VideoTemplate.java
+++ src/org/mythtv/services/api/video/impl/VideoTemplate.java
@@ -25,8 +25,13 @@ import java.util.List;
 
 import org.mythtv.services.api.video.BlurayInfo;
 import org.mythtv.services.api.video.VideoLookup;
+import org.mythtv.services.api.video.VideoLookupList;
 import org.mythtv.services.api.video.VideoMetadataInfo;
+import org.mythtv.services.api.video.VideoMetadataInfoList;
 import org.mythtv.services.api.video.VideoOperations;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -47,8 +52,15 @@ public class VideoTemplate extends AbstractVideoOperations implements VideoOpera
 	 */
 	@Override
 	public boolean addVideo( String filename, String hostname ) {
-		// TODO Auto-generated method stub
-		return false;
+
+		LinkedMultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
+		parameters.add( "FileName", filename );
+		parameters.add( "HostName", hostname );
+
+		ResponseEntity<Boolean> responseEntity = restTemplate.exchange( buildUri( "AddVideo", parameters ), HttpMethod.GET, getRequestEntity(), Boolean.class );
+		Boolean added = responseEntity.getBody();
+
+		return added;
 	}
 
 	/* (non-Javadoc)
@@ -56,8 +68,14 @@ public class VideoTemplate extends AbstractVideoOperations implements VideoOpera
 	 */
 	@Override
 	public BlurayInfo getBluray( String path ) {
-		// TODO Auto-generated method stub
-		return null;
+
+		LinkedMultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
+		parameters.add( "Path", path );
+
+		ResponseEntity<BlurayInfo> responseEntity = restTemplate.exchange( buildUri( "GetBluray", parameters ), HttpMethod.GET, getRequestEntity(), BlurayInfo.class );
+		BlurayInfo blurayInfo = responseEntity.getBody();
+
+		return blurayInfo;
 	}
 
 	/* (non-Javadoc)
@@ -65,8 +83,14 @@ public class VideoTemplate extends AbstractVideoOperations implements VideoOpera
 	 */
 	@Override
 	public VideoMetadataInfo getVideo( int id ) {
-		// TODO Auto-generated method stub
-		return null;
+
+		LinkedMultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
+		parameters.add( "Id", "" + id );
+
+		ResponseEntity<VideoMetadataInfo> responseEntity = restTemplate.exchange( buildUri( "GetVideoList", parameters ), HttpMethod.GET, getRequestEntity(), VideoMetadataInfo.class );
+		VideoMetadataInfo videoMetadataInfo = responseEntity.getBody();
+
+		return videoMetadataInfo;
 	}
 
 	/* (non-Javadoc)
@@ -74,8 +98,14 @@ public class VideoTemplate extends AbstractVideoOperations implements VideoOpera
 	 */
 	@Override
 	public VideoMetadataInfo getVideByFilename( String filename ) {
-		// TODO Auto-generated method stub
-		return null;
+
+		LinkedMultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
+		parameters.add( "FileName", filename );
+
+		ResponseEntity<VideoMetadataInfo> responseEntity = restTemplate.exchange( buildUri( "GetVideoByFileName", parameters ), HttpMethod.GET, getRequestEntity(), VideoMetadataInfo.class );
+		VideoMetadataInfo videoMetadataInfo = responseEntity.getBody();
+
+		return videoMetadataInfo;
 	}
 
 	/* (non-Javadoc)
@@ -83,8 +113,22 @@ public class VideoTemplate extends AbstractVideoOperations implements VideoOpera
 	 */
 	@Override
 	public List<VideoMetadataInfo> getVideoList( boolean descending, int startIndex, int count ) {
-		// TODO Auto-generated method stub
-		return null;
+
+		LinkedMultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
+		parameters.add( "Descending", Boolean.toString( descending ) );
+		
+		if( startIndex > 0 ) {
+			parameters.add( "StartIndex", "" + startIndex );
+		}
+
+		if( count > 0 ) {
+			parameters.add( "Count", "" + count );
+		}
+
+		ResponseEntity<VideoMetadataInfoList> responseEntity = restTemplate.exchange( buildUri( "GetVideoList", parameters ), HttpMethod.GET, getRequestEntity(), VideoMetadataInfoList.class );
+		VideoMetadataInfoList videoMetadataInfoList = responseEntity.getBody();
+
+		return videoMetadataInfoList.getVideoMetadataInfos().getVideoMetadataInfos();
 	}
 
 	/* (non-Javadoc)
@@ -92,8 +136,30 @@ public class VideoTemplate extends AbstractVideoOperations implements VideoOpera
 	 */
 	@Override
 	public List<VideoLookup> lookupVideo( String title, String subtitle, String inetRef, int season, int episode, String grabberType, boolean allowGeneric ) {
-		// TODO Auto-generated method stub
-		return null;
+
+		LinkedMultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
+		parameters.add( "Title", title );
+		
+		if( null != subtitle && !"".equals( subtitle ) ) {
+			parameters.add( "Subtitle", subtitle );
+		}
+
+		if( null != inetRef && !"".equals( inetRef ) ) {
+			parameters.add( "Inetref", inetRef );
+		}
+
+		if( season > 0 ) {
+			parameters.add( "Season", "" + season );
+		}
+
+		if( episode > 0 ) {
+			parameters.add( "Episode", "" + episode );
+		}
+
+		ResponseEntity<VideoLookupList> responseEntity = restTemplate.exchange( buildUri( "LookupVideo", parameters ), HttpMethod.GET, getRequestEntity(), VideoLookupList.class );
+		VideoLookupList videoLookupList = responseEntity.getBody();
+
+		return videoLookupList.getVideoLookups().getVideoLookups();
 	}
 
 	/* (non-Javadoc)
@@ -101,8 +167,14 @@ public class VideoTemplate extends AbstractVideoOperations implements VideoOpera
 	 */
 	@Override
 	public boolean removeVideoFromDatabase( int id ) {
-		// TODO Auto-generated method stub
-		return false;
+
+		LinkedMultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
+		parameters.add( "Id", "" + id );
+		
+		ResponseEntity<Boolean> responseEntity = restTemplate.exchange( buildUri( "RemoveVideoFromDB", parameters ), HttpMethod.GET, getRequestEntity(), Boolean.class );
+		Boolean added = responseEntity.getBody();
+
+		return added;
 	}
 	
 }
