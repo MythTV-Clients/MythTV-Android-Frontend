f4b8751278c3569d8e9faa932e7ca0bd65214d3d
Daniel Frey <dfrey@knoll.com>
2012-06-05 10:04:18 -0400

    refactored to support pre-honeycomb devices

--- src/org/mythtv/client/ui/AbstractLocationAwareFragmentActivity.java
+++ src/org/mythtv/client/ui/AbstractLocationAwareFragmentActivity.java
@@ -5,6 +5,7 @@ package org.mythtv.client.ui;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -65,9 +66,11 @@ public abstract class AbstractLocationAwareFragmentActivity extends AbstractMyth
 	protected void setupActionBar() {
 		Log.v( TAG, "setupActionBar : enter" );
 
-		ActionBar actionBar = getActionBar();
-		actionBar.setDisplayHomeAsUpEnabled( true );
-
+		if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ) {
+			ActionBar actionBar = getActionBar();
+			actionBar.setDisplayHomeAsUpEnabled( true );
+		}
+		
 		Log.v( TAG, "setupActionBar : exit" );
 	}
 
