bf2be36810de0897d9b1194cf2499c75be7de55d
Daniel Frey <dfrey@knoll.com>
2012-10-06 15:33:13 -0400

    updated license via maven plugin

--- src/org/mythtv/client/ui/setup/capture/CaptureCardsActivity.java
+++ src/org/mythtv/client/ui/setup/capture/CaptureCardsActivity.java
@@ -1,21 +1,20 @@
 /**
- *  This file is part of MythTV for Android
- * 
- *  MythTV for Android is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This file is part of MythTV Android Frontend
  *
- *  MythTV for Android is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * MythTV Android Frontend is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with MythTV for Android.  If not, see <http://www.gnu.org/licenses/>.
- *   
- * This software can be found at <https://github.com/MythTV-Android/mythtv-for-android/>
+ * MythTV Android Frontend is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
+ * You should have received a copy of the GNU General Public License
+ * along with MythTV Android Frontend.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This software can be found at <https://github.com/MythTV-Clients/MythTV-Android-Frontend/>
  */
 package org.mythtv.client.ui.setup.capture;
 
