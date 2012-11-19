d237b65f85b255b26e7a29c8dd176de28c692651
Daniel Frey <dfrey@knoll.com>
2012-11-19 14:02:18 -0500

    advanced db version to remove test backend connections

--- src/org/mythtv/db/DatabaseHelper.java
+++ src/org/mythtv/db/DatabaseHelper.java
@@ -47,7 +47,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	private static final String TAG = DatabaseHelper.class.getSimpleName();
 	
 	private static final String DATABASE_NAME = "mythtvdb";
-	private static final int DATABASE_VERSION = 80;
+	private static final int DATABASE_VERSION = 81;
 
 	public DatabaseHelper( Context context ) {
 		super( context, DATABASE_NAME, null, DATABASE_VERSION );
@@ -103,8 +103,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	public void onUpgrade( SQLiteDatabase db, int oldVersion, int newVersion ) {
 		Log.v( TAG, "onUpgrade : enter" );
 
-		if( oldVersion < 80 ) {
-			Log.v( TAG, "onUpgrade : upgrading to db version 80" );
+		if( oldVersion < 81 ) {
+			Log.v( TAG, "onUpgrade : upgrading to db version 81" );
 
 			onCreate( db );
 
