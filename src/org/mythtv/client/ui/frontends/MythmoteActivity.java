cfd2b4b43f30f45b162d52620c45daccb2c13756
Daniel Frey <dfrey@knoll.com>
2012-12-09 13:08:12 -0500

    cleaned up imports

--- src/org/mythtv/client/ui/frontends/MythmoteActivity.java
+++ src/org/mythtv/client/ui/frontends/MythmoteActivity.java
@@ -32,7 +32,6 @@ import android.os.PowerManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 
