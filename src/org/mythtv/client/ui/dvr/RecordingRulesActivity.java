97b447a8c9a9a07a85f6f55e421ba72a5ea6bc32
Daniel Frey <dfrey@knoll.com>
2012-12-09 13:07:17 -0500

    cleaned up imports

--- src/org/mythtv/client/ui/dvr/RecordingRulesActivity.java
+++ src/org/mythtv/client/ui/dvr/RecordingRulesActivity.java
@@ -25,7 +25,6 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
-import android.view.Menu;
 
 /**
  * @author Daniel Frey
