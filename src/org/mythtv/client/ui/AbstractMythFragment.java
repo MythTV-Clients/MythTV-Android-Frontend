f4e88efe6845869b3f599a3af2882454887e5f96
Daniel Frey <dfrey@knoll.com>
2013-01-13 10:50:36 -0500

    check for null application context once activity has ended and async tasks are still running

--- src/org/mythtv/client/ui/AbstractMythFragment.java
+++ src/org/mythtv/client/ui/AbstractMythFragment.java
@@ -32,7 +32,13 @@ public abstract class AbstractMythFragment extends Fragment implements MythtvApp
     // MythActivity methods
     //***************************************
 	public MainApplication getMainApplication() {
-		return (MainApplication) getActivity().getApplicationContext();
+		
+		if( null != getActivity() ) {
+			return (MainApplication) getActivity().getApplicationContext();
+		} else {
+			return null;
+		}
+	
 	}
 
 }
