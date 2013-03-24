bed228f9d643a7c4e1d0687fbe3fc9a4bdd4d549
Thomas G. Kenny Jr <tom8oe@gmail.com>
2013-03-24 09:43:37 -0400

    * Added backend connection status to BackendStatusFragment.

--- src/org/mythtv/client/ui/BackendStatusFragment.java
+++ src/org/mythtv/client/ui/BackendStatusFragment.java
@@ -1,23 +1,47 @@
 package org.mythtv.client.ui;
 
 import org.mythtv.R;
+import org.mythtv.client.ui.preferences.LocationProfile;
+import org.mythtv.db.preferences.LocationProfileDaoHelper;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class BackendStatusFragment extends AbstractMythFragment {
 
+	private LocationProfileDaoHelper mLocationProfileDaoHelper = LocationProfileDaoHelper.getInstance();
+	private LocationProfile mLocationProfile;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		
 		View v = inflater.inflate(R.layout.fragment_backend_status, container, false);
 		
+		if(null != v){
+			TextView tView = (TextView)v.findViewById(R.id.textview_status);
+			if(null != tView){
+				tView.setText(this.getStatusText());
+			}
+		}
+		
 		return v;
 	}
 
+	
+	
+	private String getStatusText(){
+		mLocationProfile = mLocationProfileDaoHelper.findConnectedProfile( getActivity() );
+		
+		if(null == mLocationProfile){
+			return "Backend profile is not selected";
+		}
+		
+		return  (mLocationProfile.isConnected() ? "Connected to " : "NOT Connected to ") + mLocationProfile.getName();
+	}
 
 
 }
