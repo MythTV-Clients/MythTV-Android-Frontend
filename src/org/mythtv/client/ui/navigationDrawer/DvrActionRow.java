dd7f73d85bba4317511ed6ab3eef8a63f3e97aa0
Thomas G. Kenny Jr <tom8oe@gmail.com>
2013-07-08 14:43:07 -0400

    * Changed nav item text colors * Added images to nav drawer action items

--- src/org/mythtv/client/ui/navigationDrawer/DvrActionRow.java
+++ src/org/mythtv/client/ui/navigationDrawer/DvrActionRow.java
@@ -3,7 +3,10 @@
  */
 package org.mythtv.client.ui.navigationDrawer;
 
+import org.mythtv.R;
 import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * @author dmfrey
@@ -15,4 +18,13 @@ public class DvrActionRow extends ActionRow {
 		super( context, action );
 	}
 
+	@Override
+	public View getView(View convertView) {
+		View view = super.getView(convertView);
+		
+		ImageView imgView = (ImageView)view.findViewById(R.id.navigation_drawer_action_icon);
+		imgView.setImageResource(R.drawable.ic_recordings_default);
+		
+		return view;
+	}
 }
