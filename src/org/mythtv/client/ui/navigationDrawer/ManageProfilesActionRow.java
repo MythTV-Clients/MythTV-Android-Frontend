707f4ed454dd2fd054bcdd7dae6b1ddfaedf00aa
Thomas G. Kenny Jr <tom8oe@gmail.com>
2013-08-28 12:07:30 -0400

    * Added preferences icon to manage profiles. Open to a different icon.

--- src/org/mythtv/client/ui/navigationDrawer/ManageProfilesActionRow.java
+++ src/org/mythtv/client/ui/navigationDrawer/ManageProfilesActionRow.java
@@ -3,7 +3,11 @@
  */
 package org.mythtv.client.ui.navigationDrawer;
 
+import org.mythtv.R;
+
 import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * @author dmfrey
@@ -13,6 +17,16 @@ public class ManageProfilesActionRow extends ActionRow {
 
 	public ManageProfilesActionRow( Context context, String action ) {
 		super( context, action );
+	}
+	
+	@Override
+	public View getView(View convertView) {
+		View view = super.getView(convertView);
+		
+		ImageView imgView = (ImageView)view.findViewById(R.id.navigation_drawer_action_icon);
+		imgView.setImageResource(R.drawable.ic_menu_preferences);
+		
+		return view;
 	}
 
 }
