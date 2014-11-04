c2c971ed691d295314c45e78e75e1f19c6e687dd
Bill Meek <keemllib@gmail.com>
2014-11-04 14:09:29 -0600

    3rd try, for 1.30

--- app/build/generated/source/buildConfig/debug/org/mythtv/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/org/mythtv/BuildConfig.java
@@ -8,7 +8,7 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "org.mythtv.debug";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 13;
+  public static final int VERSION_CODE = 99;
   public static final String VERSION_NAME = "1.30.1";
   /**
    * @deprecated Use {@link #APPLICATION_ID}
