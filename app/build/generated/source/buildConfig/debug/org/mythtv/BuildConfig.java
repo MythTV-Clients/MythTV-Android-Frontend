31aee26f7d7f78cf3dcf7659db77aa43ecce4631
Bill Meek <keemllib@gmail.com>
2014-11-04 13:17:42 -0600

    Gradle conversion commit for v1.30

---
+++ app/build/generated/source/buildConfig/debug/org/mythtv/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.mythtv;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.mythtv.debug";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 13;
+  public static final String VERSION_NAME = "1.30.1";
+  /**
+   * @deprecated Use {@link #APPLICATION_ID}
+   */
+  @Deprecated
+  public static final String PACKAGE_NAME = "org.mythtv.debug";
+}
---
+++ app/build/generated/source/buildConfig/test/debug/org/mythtv/debug/test/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.mythtv.debug.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.mythtv.debug.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "1.30.1";
+  /**
+   * @deprecated Use {@link #APPLICATION_ID}
+   */
+  @Deprecated
+  public static final String PACKAGE_NAME = "org.mythtv.debug.test";
+}
